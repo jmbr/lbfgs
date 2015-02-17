246079de83b9aa7370e1a2632c716f423c78e23e
Juan M. Bello Rivas <jmbr@superadditive.com>
2015-02-17 12:15:24 -0600

    Improved Fortran/C interface.

--- lbfgs.h
+++ lbfgs.h
@@ -1,14 +1,19 @@
-#pragma once
+#ifndef LBFGS_H
+#define LBFGS_H
 
-extern "C" int lbfgs_(int *n,
-                      int *m,
-                      double *x,
-                      double *f,
-                      double *g,
-                      logical *diagco,
-                      double *diag,
-                      int *iprint,
-                      double *eps,
-                      double *xtol,
-                      double *w,
-                      int *iflag);
+#include "fc.h"
+
+extern "C" int FC_GLOBAL(lbfgs, LBFGS)(int *n,
+                                       int *m,
+                                       double *x,
+                                       double *f,
+                                       double *g,
+                                       int *diagco,
+                                       double *diag,
+                                       int *iprint,
+                                       double *eps,
+                                       double *xtol,
+                                       double *w,
+                                       int *iflag);
+
+#endif
