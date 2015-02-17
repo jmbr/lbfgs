dc92c88d19673093ea695d0b7fa3f99eb08e00b4
Juan M. Bello Rivas <jmbr@superadditive.com>
2015-02-17 12:26:27 -0600

    Another improvement to the usability of the C / Fortran interface.

--- lbfgs.h
+++ lbfgs.h
@@ -3,17 +3,17 @@
 
 #include "fc.h"
 
-extern "C" int FC_GLOBAL(lbfgs, LBFGS)(int *n,
-                                       int *m,
-                                       double *x,
-                                       double *f,
-                                       double *g,
-                                       int *diagco,
-                                       double *diag,
-                                       int *iprint,
-                                       double *eps,
-                                       double *xtol,
-                                       double *w,
-                                       int *iflag);
+extern "C" int lbfgs(int *n,
+                     int *m,
+                     double *x,
+                     double *f,
+                     double *g,
+                     int *diagco,
+                     double *diag,
+                     int *iprint,
+                     double *eps,
+                     double *xtol,
+                     double *w,
+                     int *iflag);
 
 #endif
